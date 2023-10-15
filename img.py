5ce7d96174387e863eea5853c5ad57631a67b334
rlagns1234 <tonyk0521@naver.com>
2023-10-16 00:21:25 +0900

    Feat: 미사일 속도/개수 아이템, 피격 후 무적시간 1초 (미사일 아이템 등장확률: 게임 시작후 180초까지 30초마다 0.5%씩 증가, 생명 아이템 등장확률: 1%)

---
+++ img.py
@@ -0,0 +1,11 @@
+from PIL import Image
+
+#이미지 확장자, 크기 조정 프로그램
+im = Image.open('Galaga\\Image\\speed.png') #수정할 이미지 경로
+print(im.size)  #수정할 이미지의 크기 출력
+
+# Thumbnail 이미지 생성
+size = (18, 18) #조정할 이미지 크기
+im.thumbnail(size)  #설정한 크기의 새로운 이미지 생성
+
+im.save('Galaga\\Image\\speed.png') #새로운 이미지 경로/이름 설정 및 저장
